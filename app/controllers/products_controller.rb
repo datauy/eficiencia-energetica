3526355717b60b8839fc5879430576299ec1ef57
Matis Masters <matis@craftedcode.com>
2016-03-05 19:47:28 -0300

    Number of years calculations is broken on filtering

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,14 @@
 class ProductsController < ApplicationController
   def index
-    if(params[:litres])
-      litres = params[:litres].split(',')
-      @products = Product.where(litres: litres[0]..litres[1])
-    else
-      @products = Product.all
-    end
-  end
-
-  def filter
-    permitted_params
-
+    litres = permitted_params[:litres].split(',') if(permitted_params[:litres])
     query = Product
-    query.where(brand: permitted_params[:brand]) unless permitted_params[:brand].present?
-    query.where(model: permitted_params[:model]) unless permitted_params[:model].present?
+
+    query = query.where(litres: litres[0]..litres[1]) if permitted_params[:litres].present?
+    query = query.where(brand: permitted_params[:brand]) if permitted_params[:brand].present?
+    query = query.where(model: permitted_params[:efficiency_class]) if permitted_params[:efficiency_class].present?
 
     @number_of_years = permitted_params[:number_of_years].to_i || 2
+
     @products = query.all.map do |product|
       product.calculated_yearly_cost =
         product.yearly_cost(@number_of_years)
@@ -23,12 +16,14 @@ class ProductsController < ApplicationController
       product
     end
 
-    render :index
+    @brands = Product.select('DISTINCT products.brand').map(&:brand)
+    @efficiency_classes = Product.select('DISTINCT efficiency_class').map(&:efficiency_class)
+    @number_of_years_list = [1, 2, 5, 10, 20]
   end
 
   private
 
   def permitted_params
-    params.permit(:brand, :model, :number_of_years)
+    params.permit(:brand, :efficiency_class, :number_of_years, :litres)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get '/products_filter' => 'products#filter'
-
   resources :products, only: [:index, :show]
   get '/products/:litres' => 'products#index'
   root 'home#index'
